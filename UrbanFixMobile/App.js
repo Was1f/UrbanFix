6d3890e2d7e592aa4474a8ca973fe80dd6564f7e
Shadman Sakib <shadmanshafin49@gmail.com>
2025-08-29 11:33:26 +0600

    faulty files

--- UrbanFixMobile/App.js
+++ UrbanFixMobile/App.js
@@ -13,6 +13,7 @@ import CommunityHome from './app/community';
 import EmergencyContacts from './app/emergency-contacts';
 import AnnouncementsList from './app/announcements-list';
 import SplashScreen from './app/SplashScreen';
+import ViewPublicProfile from './app/ViewPublicProfile';
 
 // NID verification screens
 import NidVerifyScreen from './app/nidVerifyScreen';
@@ -57,6 +58,7 @@ function AppNavigator() {
           <Stack.Screen name="CommunityHome" component={CommunityHome} />
           <Stack.Screen name="EmergencyContacts" component={EmergencyContacts} />
           <Stack.Screen name="AnnouncementsList" component={AnnouncementsList} />
+           {/* <Stack.Screen name="ViewPublicProfile" component={ViewPublicProfile} />  */}
 
 
           {/* NID verification flow */}
